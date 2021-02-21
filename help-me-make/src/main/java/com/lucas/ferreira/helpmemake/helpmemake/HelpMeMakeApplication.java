ce907f95db9fb92bb3deae84e0f411e303184ac4
Lucas Ferreira D. Silva <lucferreira14@gmail.com>
2021-02-21 02:11:26 -0300

    init commit

---
+++ help-me-make/src/main/java/com/lucas/ferreira/helpmemake/helpmemake/HelpMeMakeApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.ferreira.helpmemake.helpmemake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpMeMakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpMeMakeApplication.class, args);
+	}
+
+}
---
+++ help-me-make/src/test/java/com/lucas/ferreira/helpmemake/helpmemake/HelpMeMakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.ferreira.helpmemake.helpmemake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpMeMakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
